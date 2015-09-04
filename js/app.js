90fa14b4c180c84905b0dfad441d3256e2c412a5
hwahle <helenwahle@gmail.com>
2015-09-03 21:40:21 -0700

    removed unneccessary bracket

--- js/app.js
+++ js/app.js
@@ -11,30 +11,21 @@ $('.ryu-ready').show();
 	playHadouken();
 	 $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show().animate(
+    $('.hadouken').finish().show().animate(
   {'left': '1020px'},
   500,
   function() {
     $(this).hide();
     $(this).css('left', '520px');
-  }
-);
+  });
 })
 .mouseup(function() {
 	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
-})
-$('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $(this).hide();
-      $(this).css('left', '520px');
-    })
+	});
+});
   function playHadouken () {
   	$('#hadouken-sound')[0].volume = 0.5;
   	$('#hadouken-sound')[0].load();
   	$('#hadouken-sound')[0].play();
 }
-});
